e8ea699d5ca1e1783c4d4d7fb8cffbe55ce66be4
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-28 18:36:49 -0300

    fix: fix username

--- app/backend/src/database/migrations/users-create.js
+++ app/backend/src/database/migrations/users-create.js
@@ -7,7 +7,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userame: {
+      username: {
         allowNull: false,
         type: Sequelize.STRING
       },
