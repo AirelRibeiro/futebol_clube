830b605813155cee8f8edecc7684e76a51fe5414
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-28 18:45:07 -0300

    feat: implements UserModel

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id?: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'User',
+    timestamps: false,
+  },
+);
+
+export default User;
