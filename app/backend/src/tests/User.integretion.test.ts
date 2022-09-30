4092544981cf3110eb8afb8c40015711a23b5bfd
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-30 19:32:38 -0300

    test: imports Ilogin and UserModel to test how working login route

--- app/backend/src/tests/User.integretion.test.ts
+++ app/backend/src/tests/User.integretion.test.ts
@@ -3,4 +3,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import * as Sinon from 'sinon';
 import { app } from '../app';
+import User from '../database/models/UserModel';
+import ILogin from '../interfaces/ILogin.interface';
+
 chai.use(chaiHttp);
