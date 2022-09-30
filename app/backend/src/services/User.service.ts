c537825f1c06e580a8b923f9768c0679ed9a39d6
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-30 19:44:14 -0300

    feat: implements login method

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -1,5 +1,20 @@
+import IJWTToken from '../interfaces/IJWT.interface';
+import User from '../database/models/UserModel';
+import ILogin from '../interfaces/ILogin.interface';
+import generateToken from './helpers/tokenFunctions';
+
 class UserService {
   constructor(private userModel: typeof User) { }
+
+  async login(userLogin: ILogin): Promise<IJWTToken> {
+    const user = await this.userModel.findOne(
+      { where: { email: userLogin.email } },
+    );
+    if (!user) throw new Error('invalidLoginUser');
+    const token = generateToken(user.id, user.username, user.role);
+
+    return token;
+  }
 }
 
 export default UserService;
