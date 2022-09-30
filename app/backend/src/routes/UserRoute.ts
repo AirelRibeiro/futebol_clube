b9ca586dedaa16852f29117414ad73b6f48a9b7b
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-30 19:51:15 -0300

    feat: implements usersRoute

---
+++ app/backend/src/routes/UserRoute.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import User from '../database/models/UserModel';
+import UserService from '../services/User.service';
+import UserController from '../controllers/UserController.controller';
+import loginVAlidation from '../middlewares/loginValidation';
+
+const usersRoute = express.Router();
+
+const userController = new UserController(new UserService(User));
+
+usersRoute.post('/', loginVAlidation, (req, res, next) => {
+  userController.login(req, res, next);
+});
+
+usersRoute.get('/validate', (req, res, next) => {
+  UserController.validate(req, res, next);
+});
+
+export default usersRoute;
