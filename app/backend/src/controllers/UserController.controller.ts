cb478de10255e4a63a9fd4c450e39385e2040ae1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-30 19:46:54 -0300

    feat: implements login method

--- app/backend/src/controllers/UserController.controller.ts
+++ app/backend/src/controllers/UserController.controller.ts
@@ -1,5 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
 class UserController {
   constructor(private userService: UserService) {}
+
+  async login(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const token = await this.userService.login(req.body as ILogin);
+      return res.status(200).json(token);
+    } catch (err) {
+      next(err);
+    }
+  }
 
 
 export default UserController;
