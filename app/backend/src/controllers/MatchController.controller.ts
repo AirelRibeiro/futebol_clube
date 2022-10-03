18a478a9c7598c41479047aa7444dd31bd9bf3d2
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 17:42:03 -0300

    feat: implements findAll method

--- app/backend/src/controllers/MatchController.controller.ts
+++ app/backend/src/controllers/MatchController.controller.ts
@@ -4,6 +4,17 @@ import MatchService from '../services/Match.service';
 class MatchController {
   constructor(private matchService: MatchService) {}
 
+  async findAll(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+
+    if (!inProgress) {
+      const matches = await this.matchService.findAll();
+      return res.status(200).json(matches);
+    }
+    const matches = await this.matchService.findAllWithQuery(Boolean(inProgress));
+    return res.status(200).json(matches);
+  }
+
 }
 
 export default MatchController;
