e8198b77b9ad1231089a60db4671b28bab286349
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 17:42:42 -0300

    feat: implements updateInProgress method

--- app/backend/src/controllers/MatchController.controller.ts
+++ app/backend/src/controllers/MatchController.controller.ts
@@ -4,9 +4,29 @@ import MatchService from '../services/Match.service';
 class MatchController {
   constructor(private matchService: MatchService) {}
 
+  async findAll(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+
+    if (!inProgress) {
+      const matches = await this.matchService.findAll();
+      return res.status(200).json(matches);
+    }
+    const matches = await this.matchService.findAllWithQuery(Boolean(inProgress));
+    return res.status(200).json(matches);
+  }
+
   async updateInProgress(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const message = this.matchService.updateProgress(Number(id));
+
+    return res.status(200).json(message);
+  }
+
+  async updateGoals(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { homeTeamGoals, awayTeamGoals } = req.body;
+    const message = this.matchService
+      .updateGoals(Number(id), Number(homeTeamGoals), Number(awayTeamGoals));
 
     return res.status(200).json(message);
   }
