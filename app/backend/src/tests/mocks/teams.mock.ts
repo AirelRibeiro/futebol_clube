972d692c7c7c13546cd6ac129ab7780c5dfa9f88
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 11:05:28 -0300

    test: creates mocks to teams test

---
+++ app/backend/src/tests/mocks/teams.mock.ts
@@ -0,0 +1,29 @@
+const allTeams = [
+    { id: 1, teamName: 'The Geek Team' },
+    { id: 2, teamName: 'Brave Squad' },
+    { id: 3, teamName: 'Spring Fairies' },
+    { id: 4, teamName: 'Error Code' },
+    { id: 5, teamName: 'Barbie Team' },
+    { id: 6, teamName: 'Fantastic World Team' },
+    { id: 7, teamName: 'Rainbow Warriors' },
+    { id: 8, teamName: 'Singers Flamingos' },
+    { id: 9, teamName: 'Worker Bees' },
+    { id: 10, teamName: 'Spring Team' },
+    { id: 11, teamName: 'Summer Team' },
+    { id: 12, teamName: 'Winter Teams' },
+    { id: 13, teamName: 'Autumn Team' },
+    { id: 14, teamName: 'Sparkling Platypuses' },
+    { id: 15, teamName: 'Rainbow Prime' },
+    { id: 16, teamName: 'Truffle Chocolate' },
+    { id: 16, teamName: 'Cute Bunnies' },
+    { id: 16, teamName: 'Flying High' },
+    { id: 16, teamName: 'Sparkling' },
+    { id: 16, teamName: 'The Platypuses' },
+  ];
+
+const singleTeam = { id: 14, teamName: 'Sparkling Platypuses' }
+
+export {
+  allTeams,
+  singleTeam,
+}
