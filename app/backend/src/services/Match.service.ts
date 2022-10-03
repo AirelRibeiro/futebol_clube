70a7f1662f02c0ff105c19e99439729637a241d6
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 17:38:34 -0300

    feat: implements method findAllWithQuery

--- app/backend/src/services/Match.service.ts
+++ app/backend/src/services/Match.service.ts
@@ -14,6 +14,16 @@ class MatchService {
     return matches;
   }
 
+  async findAllWithQuery(inProgress: boolean): Promise<Match[]> {
+    const matches = await this.matchModel.findAll({
+      where: { inProgress },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return matches;
+  }
 }
 
 export default MatchService;
