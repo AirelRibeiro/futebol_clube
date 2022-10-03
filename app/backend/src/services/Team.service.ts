a64507839e9475e4ebf856540b8ec8d198d1bc71
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 11:30:44 -0300

    feat: implements findAll to get all teams

--- app/backend/src/services/Team.service.ts
+++ app/backend/src/services/Team.service.ts
@@ -3,6 +3,11 @@ import Team from '../database/models/TeamModel';
 class TeamService {
   constructor(private teamModel: typeof Team) {}
 
+  async findAll(): Promise<Team[]> {
+    const teams = await this.teamModel.findAll();
+
+    return teams;
+  }
 }
 
 export default TeamService;
