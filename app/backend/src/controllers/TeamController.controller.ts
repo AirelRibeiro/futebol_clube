244e629d82e8e5eeaafb6d66ff242bd81feee2ea
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 11:34:21 -0300

    feat: implements findAll method to gel all teams

--- app/backend/src/controllers/TeamController.controller.ts
+++ app/backend/src/controllers/TeamController.controller.ts
@@ -4,4 +4,10 @@ import TeamService from '../services/Team.service';
 export default class TeamController {
   constructor(private teamService: TeamService) {}
 
+  async findAll(_req: Request, res: Response): Promise<Response> {
+    const team = await this.teamService.findAll();
+
+    return res.status(200).json(team);
+  }
+
 }
