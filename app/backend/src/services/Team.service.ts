d72afe4b00c10b33393f48f67ad3f9e5ef11304f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 11:31:10 -0300

    feat: implements findByPk to get teams by id

--- app/backend/src/services/Team.service.ts
+++ app/backend/src/services/Team.service.ts
@@ -8,6 +8,14 @@ class TeamService {
 
     return teams;
   }
+
+  async findByPk(id: number): Promise<Team> {
+    const team = await this.teamModel.findByPk(id);
+
+    if (!team) throw new Error('teamNotFound');
+
+    return team;
+  }
 }
 
 export default TeamService;
