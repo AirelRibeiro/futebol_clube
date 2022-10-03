4164108cc6e4fc50906da103c72ef1695f7461b3
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 17:37:14 -0300

    feat: creates MatchService

--- app/backend/src/services/Match.service.ts
+++ app/backend/src/services/Match.service.ts
@@ -1,3 +1,9 @@
+import Team from '../database/models/TeamModel';
+import Match from '../database/models/MatchModel';
+
+class MatchService {
+  constructor(private matchModel: typeof Match) {}
+
 }
 
 export default MatchService;
