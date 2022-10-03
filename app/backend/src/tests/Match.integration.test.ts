dccbea52c2f3556f08f1bf37e4db7a79c2890452
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 12:32:33 -0300

    test: implements tests to GET route, that list all matches

--- app/backend/src/tests/Match.integration.test.ts
+++ app/backend/src/tests/Match.integration.test.ts
@@ -12,6 +12,35 @@ describe('Rota de partidas', () => {
 
   describe('GET para buscar todas as partidas', () => {
 
+    beforeEach(async () => {
+      sinon.stub(Match, 'findAll').resolves(allMatches as Match[]);
+    });
+    
+    afterEach(() => sinon.restore());
+    
+    it('Verifica se são retornados 40 partidas', async () => {
+      const response = await chai.request(app).get('/matches');
+    
+      chai.expect(response).to.have.status(200);
+      chai.expect(response.body).to.length(20);
+    });
+    
+    it('Verifica as partidas estão organizados por Id', async () => {
+      const response = await chai.request(app).get('/matches');
+    
+      chai.expect(response).to.have.status(200);
+    
+      response.body.forEach(({id}: {id: number}, i: number) => {
+        chai.expect(id).to.be.equal(i + 1);
+      });
+    });
+    
+    it('Verifica se todos as partidas são retornadas corretamente', async () => {
+      const response = await chai.request(app).get('/teams');
+    
+      chai.expect(response).to.have.status(200);
+      chai.expect(response.body).to.deep.equal(allMatches);
+    });
   });
 
   describe('GET para buscar partidas em andamento', () => {
