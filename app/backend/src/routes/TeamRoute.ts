8295e67db192c4ae5c8565ce432d9fcaa829b795
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 11:31:31 -0300

    feat: implements TeamRoute

--- app/backend/src/routes/TeamRoute.ts
+++ app/backend/src/routes/TeamRoute.ts
@@ -1,1 +1,14 @@
+import * as express from 'express';
+import Team from '../database/models/TeamModel';
+import TeamService from '../services/Team.service';
+import TeamController from '../controllers/TeamController.controller';
+
 require('express-async-errors');
+
+const team = new TeamController(new TeamService(Team));
+const teamRouter = express.Router();
+
+teamRouter.get('/:id', (req, res) => team.findByPk(req, res));
+teamRouter.get('/', (req, res) => team.findAll(req, res));
+
+export default teamRouter;
