d99493cc0b40d1dc045949513be8cffccafeb2cf
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 21:16:36 -0300

    feat: includes route to insert match

--- app/backend/src/routes/MatchRoute.ts
+++ app/backend/src/routes/MatchRoute.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import Match from '../database/models/MatchModel';
 import MatchService from '../services/Match.service';
 import MatchController from '../controllers/MatchController.controller';
+import verifyMatchFields from '../middlewares/authMiddleware';
 
 require('express-async-errors');
 
@@ -9,6 +10,7 @@ const match = new MatchController(new MatchService(Match));
 const matchRouter = express.Router();
 
 matchRouter.get('/', (req, res) => match.findAll(req, res));
+matchRouter.post('/', verifyMatchFields, (req, res) => match.insertMatch(req, res));
 matchRouter.patch('/:id/finish', (req, res) => match.updateInProgress(req, res));
 matchRouter.patch('/:id', (req, res) => match.updateGoals(req, res));
 
