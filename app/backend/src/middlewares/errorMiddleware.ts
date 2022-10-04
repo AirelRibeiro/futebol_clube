821d4749029a0d9574071d23e5b1400372a18a5f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-03 21:22:09 -0300

    feat: includes errors to invalidToken and equalsTeams

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -7,6 +7,9 @@ type errInformation = {
 const errorsInformation: errInformation = {
   invalidLoginUser: { message: 'Incorrect email or password', code: 401 },
   missingFields: { message: 'All fields must be filled', code: 400 },
+  teamNotFound: { message: 'There is no team with such id!', code: 404 },
+  invalidToken: { message: 'Token must be a valid token', code: 401 },
+  equalsTeams: { message: 'It is not possible to create a match with two equal teams', code: 401 },
 };
 
 export default function errorMiddleware(
