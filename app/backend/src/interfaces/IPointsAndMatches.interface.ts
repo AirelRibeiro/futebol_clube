b99ae0e5b96a4dc74e5db3d30568eb3d8a87294c
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-04 17:03:44 -0300

    feat: creates interface to matches and points object

---
+++ app/backend/src/interfaces/IPointsAndMatches.interface.ts
@@ -0,0 +1,9 @@
+interface IPointsAndMatches {
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+}
+
+export default IPointsAndMatches;
