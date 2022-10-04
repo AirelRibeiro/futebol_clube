05939c48c6417048e42246d90f6786751c766f49
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-04 11:19:31 -0300

    test: implements test of leaderboard route

--- app/backend/src/tests/LeaderBoard.integration.ts
+++ app/backend/src/tests/LeaderBoard.integration.ts
@@ -11,3 +11,28 @@ import { allTeams } from './mocks/teams.mock';
 
 chai.use(chaiHttp);
 
+describe('Rota de leaderboard', () => {
+
+  describe('GET para listar a classificação dos times', () => {
+
+    beforeEach(async () => {
+      Sinon.stub(Team, 'findAll').resolves(allTeams as Team[]);
+      Sinon.stub(Match, 'findAll').resolves(finishedMatches as Match[]);
+    });
+    
+    afterEach(() => Sinon.restore());
+
+    it('Verifica se é retornado o número correto de times', async () => {
+
+    });
+
+    it('Verifica se o código de status é 200', async () => {
+
+    });
+
+    it('Verifica se os times estão na ordem correta', async () => {
+
+    });
+  });
+});
+
